0dc88e998f0afa7a17ab006e6d126e9d05e597fb
alvarodelpalacio <adelpalgon@gmail.com>
2024-10-16 10:53:55 +0200

    Add files via upload

---
+++ EDApracticas/algoritmos.c
@@ -0,0 +1,20 @@
+/*
+ * algoritmos.c
+ *
+ *  Created on: 07/10/2013
+ *      Author: MJ Polo
+ */
+#include <math.h>
+#include "algoritmos.h"
+// Orden cuadrático
+void algA(int n)
+{ int i,j,c;
+
+	c = 1;
+	for (i=1;i<=n;i++){
+		for (j=1;j<=n;j++) {
+			c = c + 1;
+		}
+  	}
+}
+
